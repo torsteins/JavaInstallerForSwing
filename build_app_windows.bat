e22f3f7282a243f7a597f808cac414841b51a4b1
Torstein Strømme <torstein.stromme@uib.no>
2022-12-20 13:35:16 +0100

    mer malrettet losning for Windows

--- build_app_windows.bat
+++ build_app_windows.bat
@@ -1,115 +1,94 @@
 @ECHO OFF
-rem This script for packaging a Java application as a Windows
-rem installer is based on an example by dlemmermann found at
-rem https://github.com/dlemmermann/JPackageScriptFX
+REM This script for packaging a Java application as a Windows
+REM installer is based on an example by dlemmermann found at
+REM https://github.com/dlemmermann/JPackageScriptFX
 
-rem ------ ENVIRONMENT --------------------------------------------------------
-rem The script depends on various environment variables to exist in order to
-rem run properly. The java version we want to use, the location of the java
-rem binaries (java home), and the project version as defined inside the pom.xml
-rem file, e.g. 1.0-SNAPSHOT.
-rem
-rem PROJECT_VERSION: version used in pom.xml, e.g. 1.0-SNAPSHOT
-rem APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
+REM ------ ENVIRONMENT --------------------------------------------------------
+REM The script depends on various environment variables to exist in order to
+REM run properly. The location of the java binaries (java home), and the
+REM project version as defined inside the pom.xml file, e.g. 1.0-SNAPSHOT.
 
-ECHO App name: %APP_NAME%
-ECHO App package: %APP_PACKAGE%
-ECHO App vendor: %APP_VENDOR%
-ECHO App version: %APP_VERSION%
-ECHO Java home: %JAVA_HOME%
-ECHO Java version: %JAVA_VERSION%
-ECHO Main jar: %MAIN_JAR%
-ECHO Main class: %MAIN_CLASS%
-ECHO Project version: %PROJECT_VERSION%
+ECHO "required input environment variables..."
+ECHO "  APP_PACKAGE: %APP_PACKAGE%"
+ECHO "  APP_VENDOR: %APP_VENDOR%"
+REM  APP_VERSION: the application version, e.g. 1.0.0, shown in "about" dialog
+ECHO "  APP_VERSION: %APP_VERSION%"
+ECHO "  ICON_PATH: %ICON_PATH%"
+REM  Set desired installer type: "msi" "exe" (or "app-image").
+ECHO "  INSTALLER_TYPE: %INSTALLER_TYPE%"
+ECHO "  JAVA_HOME: %JAVA_HOME%"
+ECHO "  JAVA_VERSION: %JAVA_VERSION%"
+ECHO "  MAIN_JAR: %MAIN_JAR%"
+ECHO "  MAIN_CLASS: %MAIN_CLASS%"
+ECHO "  PROJECT_NAME: %PROJECT_NAME%"
+ECHO "  PROJECT_VERSION: %PROJECT_VERSION%"
 
-rem Set desired installer type: "msi" "exe" (or "app-image").
-set INSTALLER_TYPE=msi
-ECHO Installer type: %INSTALLER_TYPE%
+ECHO "computed variables..."
+SET PATH_TO_MAIN="target\classes\%MAIN_CLASS:.=\%.class"
+ECHO "  PATH_TO_MAIN: %PATH_TO_MAIN%"
+SET YEAR=%DATE:~6,4%
+ECHO "  YEAR: %YEAR%"
 
-set YEAR=%DATE:~6,4%
-ECHO Year: %YEAR%
+REM ------ SETUP DIRECTORIES AND FILES ----------------------------------------
+REM Remove previously generated java runtime and installers. Copy all required
+REM jar files into the input/libs folder.
 
-set PATH_TO_MAIN="target\classes\%MAIN_CLASS:.=\%"
-echo Path to main: %PATH_TO_MAIN%
+ECHO "setting up directories and files..."
+IF EXIST target\java-runtime RMDIR /S /Q  .\target\java-runtime
+IF EXIST target\installer RMDIR /S /Q target\installer
+MKDIR target\installer\input\libs
 
-rem ------ SETUP DIRECTORIES AND FILES ----------------------------------------
-rem Remove previously generated java runtime and installers. Copy all required
-rem jar files into the input/libs folder.
+XCOPY /S /Q target\libs\* target\installer\input\libs\
+COPY target\%MAIN_JAR% target\installer\input\libs\
 
+REM ------ REQUIRED MODULES ---------------------------------------------------
+REM Use jlink to detect all modules that are required to run the application.
+REM Starting point for the jdep analysis is the set of jars being used by the
+REM application.
 
-IF EXIST target\java-runtime rmdir /S /Q  .\target\java-runtime
-IF EXIST target\installer rmdir /S /Q target\installer
-mkdir target\installer\input\libs
-
-xcopy /S /Q target\libs\* target\installer\input\libs\
-copy target\%MAIN_JAR% target\installer\input\libs\
-
-rem ------ REQUIRED MODULES ---------------------------------------------------
-rem Use jlink to detect all modules that are required to run the application.
-rem Starting point for the jdep analysis is the set of jars being used by the
-rem application.
-
-echo detecting required modules
+ECHO "detecting required modules with jdeps..."
 
 "%JAVA_HOME%\bin\jdeps" ^
   -q ^
   --multi-release %JAVA_VERSION% ^
   --ignore-missing-deps ^
   --class-path "target\installer\input\libs\*" ^
-  --print-module-deps "%PATH_TO_MAIN%.class" > temp.txt
+  --print-module-deps "%PATH_TO_MAIN%" > temp.txt
 
-set /p detected_modules=<temp.txt
-del temp.txt
+SET /p detected_modules=<temp.txt
+DEL temp.txt
+ECHO "  detected modules: %detected_modules%"
 
-echo detected modules: %detected_modules%
+REM ------ RUNTIME IMAGE ------------------------------------------------------
+REM Use the jlink tool to create a runtime image for our application. We are
+REM doing this in a separate step instead of letting jlink do the work as part
+REM of the jpackage tool. This approach allows for finer configuration and also
+REM works with dependencies that are not fully modularized, yet.
 
-rem ------ MANUAL MODULES -----------------------------------------------------
-rem jdk.crypto.ec has to be added manually bound via --bind-services or
-rem otherwise HTTPS does not work.
-rem
-rem See: https://bugs.openjdk.java.net/browse/JDK-8221674
-rem
-rem In addition we need jdk.localedata if the application is localized.
-rem This can be reduced to the actually needed locales via a jlink parameter,
-rem e.g., --include-locales=en,de.
-rem
-rem Don't forget the leading ','!
+ECHO "creating java runtime image with jlink..."
 
-rem set manual_modules=,jdk.localedata
-set manual_modules=
-echo manual modules: %manual_modules%
-
-rem ------ RUNTIME IMAGE ------------------------------------------------------
-rem Use the jlink tool to create a runtime image for our application. We are
-rem doing this in a separate step instead of letting jlink do the work as part
-rem of the jpackage tool. This approach allows for finer configuration and also
-rem works with dependencies that are not fully modularized, yet.
-
-echo creating java runtime image
-
-call "%JAVA_HOME%\bin\jlink" ^
+CALL "%JAVA_HOME%\bin\jlink" ^
   --strip-native-commands ^
   --no-header-files ^
   --no-man-pages ^
   --compress=2 ^
   --strip-debug ^
-  --add-modules %detected_modules%%manual_modules% ^
+  --add-modules "%detected_modules%" ^
   --output target/java-runtime
-rem  --include-locales=en,de ^
 
+REM ------ PACKAGING ----------------------------------------------------------
+REM In the end we will find the package inside the target/installer directory.
+ECHO "creating install package with jpackage..."
 
-rem ------ PACKAGING ----------------------------------------------------------
-rem In the end we will find the package inside the target/installer directory.
-
-call "%JAVA_HOME%\bin\jpackage" ^
+CALL "%JAVA_HOME%\bin\jpackage" ^
   --type %INSTALLER_TYPE% ^
   --dest target/installer ^
   --input target/installer/input/libs ^
-  --name %APP_NAME%  ^
-  --main-class %MAIN_CLASS% ^
-  --main-jar %MAIN_JAR% ^
+  --name "%PROJECT_NAME%"  ^
+  --main-class "%MAIN_CLASS%" ^
+  --main-jar "%MAIN_JAR%" ^
   --runtime-image target/java-runtime ^
-  --icon src/main/logo/windows/duke.ico ^
+  --icon "%ICON_PATH%" ^
   --app-version %APP_VERSION% ^
   --vendor "%APP_VENDOR%" ^
   --copyright "Copyright © %YEAR% %APP_VENDOR%" ^
@@ -117,4 +96,4 @@ call "%JAVA_HOME%\bin\jpackage" ^
   --win-per-user-install ^
   --win-shortcut ^
   --win-menu
-rem  --java-options -Xmx2048m
+REM  --java-options -Xmx2048m
